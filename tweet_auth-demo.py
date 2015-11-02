0f5fbe8c8c8388b946b9669008bffe2a75476690
pokrant@yahoo.com <pokrant@yahoo.com>
2015-11-03 00:47:45 +0800

    add auth file

---
+++ tweet_auth-demo.py
@@ -0,0 +1,5 @@
+#twitter api oauth credentials - replace with yours
+consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_token = 'xxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
